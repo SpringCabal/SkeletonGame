b7c3eaedf159a3ca6bfb36ce8dc2aeed2dc0a5b7
silentwings <nicfreeman1209@hotmail.com>
2016-01-07 20:28:58 +0000

    fix #10, remove bloat from start_force.lua

--- LuaUI/Widgets/start_force.lua
+++ LuaUI/Widgets/start_force.lua
@@ -3,9 +3,9 @@
 function widget:GetInfo()
 	return {
 		name    = 'Start Force',
-		desc    = 'Forces the game to start and sets globallos on',
-		author  = 'GoogleFrog',
-		date    = '8 August, 2015',
+		desc    = 'sends forcestart command',
+		author  = 'Googlefrog',
+		date    = '',
 		license = 'GNU GPL v2',
         layer = 0,
 		enabled = true,
@@ -14,19 +14,4 @@ end
 
 function widget:Initialize()
 	Spring.SendCommands('forcestart')
-end
-
-function widget:GameStart()
-	-- Kill healthbars.
-	Spring.SendCommands({"showhealthbars 0", "showrezbars 0", "unbind f9 showhealthbars"})
-	
-	local cheat = Spring.IsCheatingEnabled()
-	if not cheat then
-		Spring.SendCommands('cheat')
-	end
-	Spring.SendCommands('globallos')
-	if not cheat then
-		Spring.SendCommands('cheat')
-	end
-	Spring.SendCommands('disticon 99999')
 end
